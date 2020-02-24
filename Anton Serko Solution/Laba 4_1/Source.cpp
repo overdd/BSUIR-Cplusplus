f61f0c70c1135ea8a29663770b7e0b47ba705011
overdd <antonserko@gmail.com>
2020-02-25 00:16:59 +0300

    Laba 4 ready

--- Anton Serko Solution/Laba 4_1/Source.cpp
+++ Anton Serko Solution/Laba 4_1/Source.cpp
@@ -1,62 +1,80 @@
-#include<iostream> 
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <malloc.h>
 using namespace std;
 
-int n = 3, m = 4, counter, a, b, sumA, sumB;
+int n, m, counter, sumA, sumB, * mA, * mB;
 int arrayHandler(int a[], int b[]);
 
 int main() {
 	setlocale(LC_ALL, "Russian");
 
-//enter: printf("Введите количество значений в массиве А:");
-//	cin >> n;
-//	printf("Введите количество значений в массиве B:");
-//	cin >> m;
-	//a = (int*)(malloc(n * sizeof(int)));
-	//b = (int*)(malloc(m * sizeof(int)));
-	//if (!a || !b) {
-	//	printf("Память перегружена! \nВведите количество значений в массиве А и массиве В повторно\n");
-	//	goto enter;
-	//};
-	int a[3] = {1, -2, 3};
-	int b[4] = {1, 45, 2, 1};
+enter: printf("введите количество значений в массиве а:");
+	scanf("%d", &n);
+	printf("введите количество значений в массиве b:");
+	scanf("%d", &m);
+	mA = (int*)(malloc(n * sizeof(int)));
+	mB = (int*)(malloc(m * sizeof(int)));
+	if (!mA || !mB) {
+		printf("память перегружена! \nвведите количество значений в массиве а и массиве в повторно\n");
+		goto enter;
+	};
+	//int a[3] = { 1, 2, 3 };
+	//int b[4] = { 1, 45, 2, -1 };
 
-	//printf("Введите элемент массива А:\n");
-	//for (int i = 0; i < n; i++) {
-	//	printf("[%d] =", i);
-	//	scanf_s("[%d]", &a[i]);
-	//};
-	//printf("Введите элемент массива B:\n");
-	//for (int i = 0; i < m; i++) {
-	//	printf("[%d] =", i);
-	//	scanf_s("[%d]", &b[i]);
-	//};
-	arrayHandler(a, b);
+	printf("Введите элемент массива А:\n");
+	for (int i = 0; i < n; i++) {
+		printf("[%d] = ", i);
+		scanf("%d", &mA[i]);
+	};
+	printf("Введите элемент массива B:\n");
+	for (int i = 0; i < m; i++) {
+		printf("[%d] =", i);
+		scanf("%d", &mB[i]);
+	};
+	arrayHandler(mA, mB);
 	printf("Выводим массив с наименьшим количеством отрицательных элементов, если он определен: \n");
 	switch (counter)
 	{
 	case 1: {
-			printf("Сейчас выведем массив А. В нем %d отрицательных элементов \n", sumA);
-			for (int i = 0; i < n; i++) {
-				printf("a[%d] = %d \n", i, a[i]);
-			};
+		printf("Сейчас выведем массив А. В нем %d отрицательных элементов \n", sumA);
+		for (int i = 0; i < n; i++) {
+			printf("a[%d] = %d \n", i, mA[i]);
 		};
+		printf("А теперь массив Б. В нем %d отрицательных элементов \n", sumB);
+		for (int i = 0; i < m; i++) {
+			printf("b[%d] = %d \n", i, mB[i]);
+		};
+		break;
+	};
 	case 0: {
 		printf("Сейчас выведем массив B. В нем %d отрицательных элементов \n", sumB);
 		for (int i = 0; i < m; i++) {
-			printf("b[%d] = %d \n", i, b[i]);
+			printf("b[%d] = %d \n", i, mB[i]);
 		};
-	};
+		printf("А теперь массив A. В нем %d отрицательных элементов \n", sumA);
+		for (int i = 0; i < n; i++) {
+			printf("a[%d] = %d \n", i, mA[i]);
+		};
+		break;
 	case 9: {
-			  printf("Оба массива содержат одинаковое количество отрицательных элементов. Сейчас выведем оба массива.\n");
-		  }
+		printf("Оба массива содержат одинаковое количество отрицательных элементов. Сейчас выведем оба массива.\n");
+		for (int i = 0; i < n; i++) {
+			printf("a[%d] = %d \n", i, mA[i]);
+		};
+		for (int i = 0; i < m; i++) {
+			printf("b[%d] = %d \n", i, mB[i]);
+		};
 	}
-
-
-	system("pause");
-
-	return 0;
+	}
+		  system("pause");
+		  return 0;
+	}
 }
-
 int arrayHandler(int a[], int b[]) {
 	for (int i = 0; i < n; i++) {
 		(a[i] < 0) ? sumA = sumA + 1 : 0;
--- Anton Serko Solution/Laba 4_2/Source.cpp
+++ Anton Serko Solution/Laba 4_2/Source.cpp
@@ -1,49 +1,154 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<iostream> 
-#include <stdio.h>
-#include <array>
+#include<iostream>
+#include<stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <malloc.h>
+#include <ctime> 
 
 using namespace std;
 
-int n, counter, *a, b, sumA;
+int n, counter, * mA, b, sumA, m, * arrayForBubbleSorting, * arrayForHoarSorting;
 int arrayHandler(int a[]);
+void bubbleSorter(int*, int);
+void hoarSorter(int*, int, int);
 
 int main() {
 	setlocale(LC_ALL, "Russian");
 
-	enter: printf("Введите количество значений в массиве :");
-		cin >> n;
-		a = (int*)(malloc(n * sizeof(int)));
-		if (!a) {
-			printf("Память перегружена! \nВведите количество значений в массиве А и массиве В повторно\n");
-			goto enter;
-		};
+enter: printf("Введите количество значений в массиве: ");
+	scanf("%d", &n);
+	mA = (int*)(malloc(n * sizeof(int)));
+	if (!mA) {
+		printf("Память перегружена! \nВведите количество значений в массиве повторно\n");
+		goto enter;
+	};
 
-	
+
 	printf("Введите элемент массива:\n");
 	for (int i = 0; i < n; i++) {
-		printf("[%d] =", i);
-		cin >> a[i];
+		printf("[%d] = ", i);
+		scanf("%d", &mA[i]);
 	};
-	
-	arrayHandler(a);
+
+	arrayHandler(mA);
 	printf("Сумма элементов массива после последнего отрицательного элемента = %d \n", sumA);
+
+	printf("Введите количество элементов массива для сортировки: ");
+	scanf("%d", &m);
+	arrayForBubbleSorting = new int[m];
+	arrayForHoarSorting = new int[m];
+	if (!arrayForBubbleSorting || !arrayForHoarSorting)
+	{
+		printf("Память перегружена! \nВведите количество значений в массиве повторно\n");
+		goto enter;
+	}
+	for (int i = 0; i < m; i++)							//Автоматическое заполнение массива
+	{
+		*(arrayForBubbleSorting + i) = rand();
+		*(arrayForHoarSorting + i) = rand();
+	}
+
+
+	int start_time_bubble = clock();
+	bubbleSorter(arrayForBubbleSorting, m);
+	int end_time_bubble = clock();
+	printf("Время выполнения сортировки методом пузырька	=	%d\n", end_time_bubble - start_time_bubble);
+	int start_time_hoar = clock();
+	hoarSorter(arrayForHoarSorting, 0, m - 1);
+	int end_time_hoar = clock();
+	printf("Время выполнения сортировки методом Хоара	=	%d\n", end_time_hoar - start_time_hoar);
+
+	delete[]arrayForBubbleSorting;
+	delete[]arrayForHoarSorting;
 	system("pause");
-	return 0;
+
 }
 
-int arrayHandler(int a[]) {
-		// сначала найдем индекс последнего отрицательного элемента
+
+void bubbleSorter(int* arr, int m)
+{
+	for (int i = 0; i < m - 1; i++)
+	{
+		for (int j = (m - 1); j > i; j--)		
+		{
+			if (*(arr + j - 1) > * (arr + j))		
+			{
+				int temp = *(arr + j - 1);		
+				*(arr + j - 1) = *(arr + j);
+				*(arr + j) = temp;
+			}
+		}
+	}
+}
+
+void hoarSorter(int* s_arr, int first, int last)
+{
+	int i = first, j = last, x = s_arr[(first + last) / 2];
+
+	do {
+		while (s_arr[i] < x) i++;
+		while (s_arr[j] > x) j--;
+
+		if (i <= j) {
+			if (s_arr[i] > s_arr[j]) swap(s_arr[i], s_arr[j]);
+			i++;
+			j--;
+		}
+	} while (i <= j);
+
+	if (i < last)
+		hoarSorter(s_arr, i, last);
+	if (first < j)
+		hoarSorter(s_arr, first, j);
+}
+
+
+
+//void hoarSorter(int* arr, int n0, int m)
+//{
+//	int i = n0, j = m, d;
+//	int a = *(arr + (n0 + m) / 2);
+//	while (i <= j) {
+//		for (; *(arr + i) < a; i++);
+//		{
+//			for (; *(arr + j) > a; j--);
+//			{
+//				if (i <= j)
+//				{
+//					d = *(arr + i);
+//					*(arr + i++) = *(arr + j);
+//					*(arr + j--) = d;
+//				}
+//			}
+//		}
+//	}
+//	if (n0 < j)
+//	{
+//		hoarSorter(arr, n0, j);
+//	}
+//	if (i < m)
+//	{
+//		hoarSorter(arr, i, m);
+//	}
+//
+//	for (int i = 0; i < m; i++) {
+//		printf("%d \n", arr[i]);
+//	};
+//}
+
+int arrayHandler(int mA[]) {
+
 	int lastNegativeIndex = 0;
 	sumA = 0;
 	for (int i = 0; i < n; i++) {
-		if (a[i] < 0) {
-			lastNegativeIndex = i;   // !!!!!!!!! нужно предусмотреть случай, если у массива нет отрицательных элементов
+		if (mA[i] < 0) {
+			lastNegativeIndex = i; 
 		};
-	// теперь суммируем все элементы после последнего отрицательного значения
 	}
-	for (int i = lastNegativeIndex+1; i < n; i++) {
-		sumA = sumA + a[i];
+	for (int i = lastNegativeIndex + 1; i < n; i++) {
+		sumA = sumA + mA[i];
 	}
 	return sumA;
 }
